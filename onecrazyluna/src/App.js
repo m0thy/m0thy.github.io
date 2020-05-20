01d201f4e72e88e06693bc2af707d82e8f6354b3
m0thy <m0thy@m0thys-Air.home>
2020-05-20 03:24:04 -0400

    spaceforce logo

--- onecrazyluna/src/App.js
+++ onecrazyluna/src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="https://static-cdn.jtvnw.net/jtv_user_pictures/021ec323-49a2-46da-8483-a391d3218191-profile_image-70x70.png" className="App-logo" alt="Space Force" />
         <p>
           ...OneCrazyLuna Page In Construction...
         </p>
