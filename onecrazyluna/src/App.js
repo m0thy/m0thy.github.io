3f22853af3408cf1d0d1b47e15d761140cf03ccd
m0thy <m0thy@m0thys-Air.home>
2020-05-21 13:12:49 -0400

    Revert "use spaceforce icon local"
    This reverts commit 0f91658a2e2e619ab53ce322c5f9295c8c8829ae.

--- onecrazyluna/src/App.js
+++ onecrazyluna/src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src="%PUBLIC_URL%/spaceforce.png" className="App-logo" alt="Space Force" />
+        <img src="https://static-cdn.jtvnw.net/jtv_user_pictures/021ec323-49a2-46da-8483-a391d3218191-profile_image-70x70.png" className="App-logo" alt="Space Force" />
         <p>
           ...OneCrazyLuna Page In Construction...
         </p>
